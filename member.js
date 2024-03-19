a722e414a8a8707115662ab502026e0041422a36
雷 <admin@uplauncher.xyz>
2024-03-19 07:25:33 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,12 @@
-function skillsNumber() {
-  return this.skills.length;
+function skillsMember() {
+    var member = {
+        name: 'John',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        greet: function() {
+            console.log('Hello, I am ' + this.name);
+        }
+    };
+
+    return member;
 }
